a9dd0a3e8be68a6a268a334ee7b923f031b6a291
Grabaz <fifipolopl@wp.pl>
2023-11-02 15:01:50 +0100

    Dodanie informacji ogólnikowych co należy dodać

---
+++ Portfolio/Calendar.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Portfolio
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Calendar.xaml
+    /// </summary>
+    
+    public partial class Calendar : Window
+    {
+        private Window MainWindow;
+        public Calendar(Window window = null)
+        {
+            MainWindow = window;
+            InitializeComponent();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow.Focus();
+            MainWindow.Visibility = Visibility.Visible;
+        }
+
+        
+
+        private void Main_Menu(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Focus();
+            MainWindow.Visibility= Visibility.Visible;
+            Close();
+        }
+
+        ///Add calendar
+        ///Conect to Events and Raports 
+        ///we cant do anytching about calendar only see whoat type of payments calendar is monthly
+
+       
+    }
+}
---
+++ Portfolio/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Portfolio
+{
+    
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Interaction logic for MainWindow.xaml
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Calendar(object sender, RoutedEventArgs e)
+        {
+            Window calendar = new Calendar(this);
+            calendar.Show();
+            calendar.Focus();
+            Visibility = Visibility.Hidden;
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Events(object sender, RoutedEventArgs e)
+        {
+            ///Add new Window Events
+            ///Add goto Events
+            ///show event payment and caliber to raports and calendar
+        }
+
+        private void Raports(object sender, RoutedEventArgs e)
+        {
+            ///Add new Window Raports
+            ///Add goto Raports
+            ///In raports you can download pdf Raports Day Week Month Year
+        }
+    }
+}
